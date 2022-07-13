14250015255b075498a2acf52646767ac77a7146
Lia Daiely Magalhães <liadaiely@gmail.com>
2022-07-13 19:32:22 -0300

    Other components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Header from './components/Header'
 import NotificationButton from './components/NotificationButton'
+import SalesCard from './components/SalesCard'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <>
-    <App />
-    <NotificationButton/>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+      <App />
+      <NotificationButton />
+
     </>
-  
+
   </React.StrictMode>
 )
