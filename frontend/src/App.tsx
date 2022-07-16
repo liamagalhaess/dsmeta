a3a5ca6ce9d8bb4790ab561e88ff71bfa99ca06f
Lia Daiely Magalhães <liadaiely@gmail.com>
2022-07-15 22:34:02 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,21 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
+import SalesCard from './components/SalesCard';
+
 function App() {
   return(
-   <h1>Olá mundo!</h1>
+   <>
+    <ToastContainer/>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Header from './components/Header';
-import SalesCard from './components/SalesCard';
+import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <>
-      <Header />
-      <main>
-        <section id="sales">
-          <div className="dsmeta-container">
-            <SalesCard />
-          </div>
-        </section>
-      </main>
+      <App/>
     </>
 
   </React.StrictMode>
